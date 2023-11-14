22ab85bc5f26fa710e2ea76126938442f68c652d
Weldarn <marc-antoine.t@hotmail.com>
2023-11-14 07:09:23 +0100

    code bug

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -25,4 +25,31 @@ class ProfileController extends AbstractController
             'scores' => $user->getScores(),
         ]);
     }
+
+    #[Route('/api/user_info', name: 'api_user_info', methods: ['GET'])]
+    public function userInfo(): Response
+    {
+        $user = $this->security->getUser();
+
+        if (!$user) {
+            // Retourner une réponse d'erreur si l'utilisateur n'est pas authentifié
+            return $this->json(['error' => 'User not authenticated'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        // Récupérer et formater les scores de l'utilisateur
+        $scores = [];
+        foreach ($user->getGameScores() as $gameScore) {
+            $scores[] = [
+                'scoreChampionEasy' => $gameScore->getScoreChampionEasy(),
+                'scoreChampionHard' => $gameScore->getScoreChampionHard(),
+                'scoreObject' => $gameScore->getScoreObject(),
+            ];
+        }
+
+        // Retourner les informations de l'utilisateur
+        return $this->json([
+            'username' => $user->getUsername(),
+            'scores' => $scores,
+        ]);
+    }
 }
